0a7efe9ea1efc568d87ebe1a9be670ffa4d2d4b8
Nick Young <nick.young@auckland.ac.nz>
2025-05-26 10:22:58 +1200

    generate README

--- fetch.py
+++ fetch.py
@@ -8,9 +8,15 @@ s = HTMLSession()
 BASE_URL = "https://merge-mansion.fandom.com/"
 areas = s.get(f"{BASE_URL}/wiki/Areas")
 areas = areas.html.find("table", first=True).find("tr>td:nth-child(3)>a")[1:]
+index = ""
 for area in tqdm(areas):
     svgs = s.get(BASE_URL + area.attrs['href']).html.find("a[href$=svg]")
     for svg in svgs:
         svg_link = BASE_URL + svg.attrs["href"]
-        with open(os.path.basename(svg_link), "wb") as f:
+        filename = os.path.basename(svg_link)
+        with open(filename, "wb") as f:
             f.write(s.get(svg_link).content)
+        index += f"[{filename}]({filename})  \n"
+
+with open("README.md", "w") as f:
+    f.write(index)
